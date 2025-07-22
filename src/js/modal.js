2dba899495064b308f5d3f76a7f0978545936ba5
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-07-23 00:22:15 +0200

    update modal window
    update js, add three buttons for test

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,16 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
       text: document.querySelector('[data-modal-text]'),
       price: document.querySelector('[data-modal-price]'),
       input: document.querySelectorAll('.modal-form__input'),
+      checkoutBasic: document.querySelector('[data-checkout-basic]'),
+      checkoutStandart: document.querySelector('[data-checkout-standart]'),
+      checkoutPro: document.querySelector('[data-checkout-pro]'),
     };
 
-    refs.openModalBtn?.addEventListener('click', () => {
+    refs.checkoutBasic?.addEventListener('click', () => {
+      refs.modal.classList.remove('is-hidden');
+      refs.text.textContent = 'One Session';
+      refs.price.textContent = '$10';
+    });
+
+    refs.checkoutStandart?.addEventListener('click', () => {
       refs.modal.classList.remove('is-hidden');
       refs.text.textContent = '8 Sessions Package';
       refs.price.textContent = '$65';
     });
 
+    refs.checkoutPro?.addEventListener('click', () => {
+      refs.modal.classList.remove('is-hidden');
+      refs.text.textContent = '12 Sessions Package';
+      refs.price.textContent = '$80';
+    });
+
     refs.closeModalBtn?.addEventListener('click', () => {
       refs.modal.classList.add('is-hidden');
+    });
+
+    refs.modal?.addEventListener('click', event => {
+      if (event.target === refs.modal) {
+        refs.modal.classList.add('is-hidden');
+      }
     });
   })();
 });
