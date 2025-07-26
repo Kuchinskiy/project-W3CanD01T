a0d8efbc52f0a342ff212514bfab115d03f61186
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-07-26 22:37:43 +0200

    init header section

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -4,14 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
       menuBtn: document.querySelector('[data-menu-btn]'),
       closeBtn: document.querySelector('[data-menu-close-btn]'),
       menu: document.querySelector('[data-menu]'),
+      navLink: document.querySelector('[data-nav-link]'),
     };
 
     navigation.menuBtn?.addEventListener('click', () => {
       navigation.menu.classList.remove('is-hidden');
+      navigation.menuBtn.classList.remove('visible');
+      navigation.closeBtn.classList.add('visible');
     });
 
     navigation.closeBtn?.addEventListener('click', () => {
       navigation.menu.classList.add('is-hidden');
+      navigation.menuBtn.classList.add('visible');
+      navigation.closeBtn.classList.remove('visible');
+    });
+
+    navigation.navLink?.addEventListener('click', () => {
+      navigation.menu.classList.add('is-hidden');
+      navigation.menuBtn.classList.add('visible');
+      navigation.closeBtn.classList.remove('visible');
     });
   })();
 });
