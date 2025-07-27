2e0aeb47ad4ef80341748bb81899ee67761d197b
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-07-27 17:51:53 +0200

    update scripts

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,43 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-      text: document.querySelector('[data-modal-text]'),
-      price: document.querySelector('[data-modal-price]'),
-      input: document.querySelectorAll('.modal-form__input'),
-      checkoutBasic: document.querySelector('[data-checkout-basic]'),
-      checkoutStandard: document.querySelector('[data-checkout-standard]'),
-      checkoutPro: document.querySelector('[data-checkout-pro]'),
-    };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    text: document.querySelector('[data-modal-text]'),
+    price: document.querySelector('[data-modal-price]'),
+    input: document.querySelectorAll('.modal-form__input'),
+    checkoutBasic: document.querySelector('[data-checkout-basic]'),
+    checkoutStandard: document.querySelector('[data-checkout-standard]'),
+    checkoutPro: document.querySelector('[data-checkout-pro]'),
+  };
 
-    refs.checkoutBasic?.addEventListener('click', () => {
-      refs.modal.classList.remove('is-hidden');
-      refs.text.textContent = 'One Session';
-      refs.price.textContent = '$10';
-    });
+  refs.checkoutBasic?.addEventListener('click', () => {
+    refs.modal.classList.remove('is-hidden');
+    refs.text.textContent = 'One Session';
+    refs.price.textContent = '$10';
+  });
 
-    refs.checkoutStandard?.addEventListener('click', () => {
-      refs.modal.classList.remove('is-hidden');
-      refs.text.textContent = '8 Sessions Package';
-      refs.price.textContent = '$65';
-    });
+  refs.checkoutStandard?.addEventListener('click', () => {
+    refs.modal.classList.remove('is-hidden');
+    refs.text.textContent = '8 Sessions Package';
+    refs.price.textContent = '$65';
+  });
 
-    refs.checkoutPro?.addEventListener('click', () => {
-      refs.modal.classList.remove('is-hidden');
-      refs.text.textContent = '12 Sessions Package';
-      refs.price.textContent = '$80';
-    });
+  refs.checkoutPro?.addEventListener('click', () => {
+    refs.modal.classList.remove('is-hidden');
+    refs.text.textContent = '12 Sessions Package';
+    refs.price.textContent = '$80';
+  });
 
-    refs.closeModalBtn?.addEventListener('click', () => {
+  refs.closeModalBtn?.addEventListener('click', () => {
+    refs.modal.classList.add('is-hidden');
+  });
+
+  refs.modal?.addEventListener('click', event => {
+    if (event.target === refs.modal) {
       refs.modal.classList.add('is-hidden');
-    });
-
-    refs.modal?.addEventListener('click', event => {
-      if (event.target === refs.modal) {
-        refs.modal.classList.add('is-hidden');
-      }
-    });
-  })();
-});
+    }
+  });
+})();
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,45 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (() => {
-    const navigation = {
-      menuBtn: document.querySelector('[data-menu-btn]'),
-      closeBtn: document.querySelector('[data-menu-close-btn]'),
-      menu: document.querySelector('[data-menu]'),
-      navLink: document.querySelector('[data-nav-link]'),
-      joinBtn: document.querySelector('[data-join-btn]'),
-      modalJoinBtn: document.querySelector('[data-modal-join-btn]'),
-    };
+(() => {
+  const navigation = {
+    menuBtn: document.querySelector('[data-menu-btn]'),
+    closeBtn: document.querySelector('[data-menu-close-btn]'),
+    menu: document.querySelector('[data-menu]'),
+    navLink: document.querySelector('[data-nav-link]'),
+    joinBtn: document.querySelector('[data-join-btn]'),
+    modalJoinBtn: document.querySelector('[data-modal-join-btn]'),
+  };
 
-    navigation.menuBtn?.addEventListener('click', () => {
-      navigation.menu.classList.remove('is-hidden');
-      navigation.menuBtn.classList.remove('visible');
-      navigation.closeBtn.classList.add('visible');
-    });
+  navigation.menuBtn?.addEventListener('click', () => {
+    navigation.menu.classList.remove('is-hidden');
+    navigation.menuBtn.classList.remove('visible');
+    navigation.closeBtn.classList.add('visible');
+  });
 
-    navigation.closeBtn?.addEventListener('click', () => {
-      navigation.menu.classList.add('is-hidden');
-      navigation.menuBtn.classList.add('visible');
-      navigation.closeBtn.classList.remove('visible');
-    });
+  navigation.closeBtn?.addEventListener('click', () => {
+    navigation.menu.classList.add('is-hidden');
+    navigation.menuBtn.classList.add('visible');
+    navigation.closeBtn.classList.remove('visible');
+  });
 
-    navigation.navLink?.addEventListener('click', () => {
-      navigation.menu.classList.add('is-hidden');
-      navigation.menuBtn.classList.add('visible');
-      navigation.closeBtn.classList.remove('visible');
-    });
+  navigation.navLink?.addEventListener('click', () => {
+    navigation.menu.classList.add('is-hidden');
+    navigation.menuBtn.classList.add('visible');
+    navigation.closeBtn.classList.remove('visible');
+  });
 
-    navigation.joinBtn?.addEventListener('click', () => {
-      navigation.menu.classList.add('is-hidden');
-      navigation.menuBtn.classList.add('visible');
-      navigation.closeBtn.classList.remove('visible');
-    });
+  navigation.joinBtn?.addEventListener('click', () => {
+    navigation.menu.classList.add('is-hidden');
+    navigation.menuBtn.classList.add('visible');
+    navigation.closeBtn.classList.remove('visible');
+  });
 
-    navigation.modalJoinBtn?.addEventListener('click', () => {
-      navigation.menu.classList.add('is-hidden');
-      navigation.menuBtn.classList.add('visible');
-      navigation.closeBtn.classList.remove('visible');
-    });
-  })();
-});
+  navigation.modalJoinBtn?.addEventListener('click', () => {
+    navigation.menu.classList.add('is-hidden');
+    navigation.menuBtn.classList.add('visible');
+    navigation.closeBtn.classList.remove('visible');
+  });
+})();
 
 const sections = document.querySelectorAll('section[id]');
 const navLinks = document.querySelectorAll('.header-nav__link');
