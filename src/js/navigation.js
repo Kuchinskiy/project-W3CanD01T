50c256556e87da60753952cc3f12ce8011fe8f31
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-07-28 20:32:44 +0200

    Refactoring navigation.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,41 +1,26 @@
 (() => {
-  const navigation = {
+  const refs = {
     menuBtn: document.querySelector('[data-menu-btn]'),
     closeBtn: document.querySelector('[data-menu-close-btn]'),
     menu: document.querySelector('[data-menu]'),
-    navLink: document.querySelector('[data-nav-link]'),
-    joinBtn: document.querySelector('[data-join-btn]'),
-    modalJoinBtn: document.querySelector('[data-modal-join-btn]'),
+    navLinks: document.querySelectorAll('[data-nav-link]'),
+    closeTriggers: document.querySelectorAll(
+      '[data-menu-close-btn], [data-nav-link], [data-join-btn], [data-modal-join-btn]'
+    ),
   };
 
-  navigation.menuBtn?.addEventListener('click', () => {
-    navigation.menu.classList.remove('is-hidden');
-    navigation.menuBtn.classList.remove('visible');
-    navigation.closeBtn.classList.add('visible');
+  refs.menuBtn?.addEventListener('click', () => {
+    refs.menu.classList.remove('is-hidden');
+    refs.menuBtn.classList.remove('visible');
+    refs.closeBtn.classList.add('visible');
   });
 
-  navigation.closeBtn?.addEventListener('click', () => {
-    navigation.menu.classList.add('is-hidden');
-    navigation.menuBtn.classList.add('visible');
-    navigation.closeBtn.classList.remove('visible');
-  });
-
-  navigation.navLink?.addEventListener('click', () => {
-    navigation.menu.classList.add('is-hidden');
-    navigation.menuBtn.classList.add('visible');
-    navigation.closeBtn.classList.remove('visible');
-  });
-
-  navigation.joinBtn?.addEventListener('click', () => {
-    navigation.menu.classList.add('is-hidden');
-    navigation.menuBtn.classList.add('visible');
-    navigation.closeBtn.classList.remove('visible');
-  });
-
-  navigation.modalJoinBtn?.addEventListener('click', () => {
-    navigation.menu.classList.add('is-hidden');
-    navigation.menuBtn.classList.add('visible');
-    navigation.closeBtn.classList.remove('visible');
+  refs.closeTriggers.forEach(el => {
+    el?.addEventListener('click', () => {
+      refs.menu.classList.add('is-hidden');
+      refs.menuBtn.classList.add('visible');
+      refs.closeBtn.classList.remove('visible');
+    });
   });
 })();
 
